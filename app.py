2a2d207d19941232d0995ecedf253bac09a6539c
Anshuman Jena <66255580+abandonedmonk@users.noreply.github.com>
2023-12-09 23:53:49 +0530

    Added the function to read the text

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+
+# Function to read from pdfs and extract the text
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        # Reading each PDF
+        pdf_reader = PdfReader(pdf)
+        
+        # Extracting text from each page
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
