f00aedbab7419666a57097104824327ac3030c23
Anshuman Jena <66255580+abandonedmonk@users.noreply.github.com>
2023-12-10 00:12:48 +0530

    Created a new function to separate the text into different groups (chunks)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 # Function to read from pdfs and extract the text
 def get_pdf_text(pdf_docs):
@@ -13,6 +14,17 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+# Dividing the text in different chunks (groups)
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator='\n',
+        chunk_size = 1000, # 1000 characters
+        chunk_overlap = 200,
+        length_function = len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 def main():
     load_dotenv()
@@ -33,7 +45,10 @@ def main():
             with st.spinner("Processing"):
                 # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
+                
+                # get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
                 
 
 if __name__ == '__main__':
