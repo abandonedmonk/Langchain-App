fa6eb73d268bb3680b09c6183ad005ef1de1b021
Anshuman Jena <66255580+abandonedmonk@users.noreply.github.com>
2023-12-09 22:42:39 +0530

    Imported dotenv to access API keys

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
-
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv()
     # Title for the application
     st.set_page_config(page_title="Chat with Conference Papers", page_icon=":newspaper:")
     
