01b88f1e8d68ce169ef8833ed786782e1b67594e
Anshuman Jena <66255580+abandonedmonk@users.noreply.github.com>
2023-12-09 23:54:39 +0530

    Created the GUI

--- app.py
+++ app.py
@@ -13,3 +13,14 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+def main():
+    load_dotenv()
+    # Title for the application
+    st.set_page_config(page_title="Chat with Conference Papers", page_icon=":newspaper:")
+    
+    st.header("Chat with Conference Papers :newspaper:")
+    st.text_input("Ask Question about your document: ")
+    
+    # The sidebar with text, button and upload option
+    with st.sidebar:
